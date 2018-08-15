21295b308108fcc673ef6d5927570f56d3de3176
Vivek <vivek192@gmail.com>
2018-08-15 22:40:29 +0530

    semantic ui start

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
 
 
 class App extends Component {
@@ -8,6 +9,12 @@ class App extends Component {
         <header className="App-header">
           
           <h1 className="App-title">Revent</h1>
+          <button className="ui icon button">
+          <i className="smile icon"></i>
+          CSS Button
+          </button>
+            <div className="ui"></div>
+          <Button icon="smile" content="React button" />
         </header>
         
       </div>
