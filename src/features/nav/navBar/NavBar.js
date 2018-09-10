9b52d7cf94dd83bf7f44ad765a73494026cda52a
Vivek Arockiaraj <vivek.arockiaraj@genomels.com>
2018-09-10 12:54:19 +0530

    Section 6 49 Routing with Redirect

--- src/features/nav/navBar/NavBar.js
+++ src/features/nav/navBar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Menu, Container, Button } from "semantic-ui-react";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink, Link, withRouter } from "react-router-dom";
 import SignedOutMenu from "../Menus/SignedOutMenu";
 import SignedInMenu from "../Menus/SignedInMenu";
 
@@ -17,6 +17,7 @@ class NavBar extends Component {
     this.setState({
       authendicated: false
     });
+    this.props.history.push("/");
   };
   render() {
     const { authendicated } = this.state;
@@ -25,21 +26,25 @@ class NavBar extends Component {
         <Menu inverted fixed="top">
           <Container>
             <Menu.Item as={Link} to="/" header>
-              <img src="assets/logo.png" alt="logo" />
+              <img src="/assets/logo.png" alt="logo" />
               Re-vents
             </Menu.Item>
             <Menu.Item as={NavLink} to="/events" name="Events" />
-            <Menu.Item as={NavLink} to="/people" name="People" />
-            <Menu.Item>
-              <Button
-                as={Link}
-                to="/createEvent"
-                floated="right"
-                positive
-                inverted
-                content="Create Event"
-              />
-            </Menu.Item>
+            {authendicated && (
+              <Menu.Item as={NavLink} to="/people" name="People" />
+            )}
+            {authendicated && (
+              <Menu.Item>
+                <Button
+                  as={Link}
+                  to="/createEvent"
+                  floated="right"
+                  positive
+                  inverted
+                  content="Create Event"
+                />
+              </Menu.Item>
+            )}
             {authendicated ? (
               <SignedInMenu signOut={this.handleSignOut} />
             ) : (
@@ -52,4 +57,4 @@ class NavBar extends Component {
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
--- src/features/users/settings/SettingDashboard.js
+++ src/features/users/settings/SettingDashboard.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import SettingNav from "./SettingNav";
+import { Switch, Route, Redirect } from "react-router-dom";
+import AboutPage from "./AboutPage";
+import BasicPage from "./BasicPage";
+import AccountPage from "./AccountPage";
+import PhotoPage from "./PhotoPage";
 
 const SettingDashboard = () => {
   return (
-    <div>
-      <h1>SettingDashboard</h1>
-    </div>
-  )
-}
+    <Grid>
+      <Grid.Column width={12}>
+        <Switch>
+          <Redirect exact from="/settings" to="/settings/basic" />
+          <Route path="/settings/basic" component={BasicPage} />
+          <Route path="/settings/account" component={AccountPage} />
+          <Route path="/settings/photos" component={PhotoPage} />
+          <Route path="/settings/about" component={AboutPage} />
+        </Switch>
+      </Grid.Column>
+      <SettingNav />
+    </Grid>
+  );
+};
 
-export default SettingDashboard
+export default SettingDashboard;
