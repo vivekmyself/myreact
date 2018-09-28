98fd0cfc59c65a5412ece1edd3289c0dcc0557a2
Vivek Arockiaraj <vivek.arockiaraj@genomels.com>
2018-09-28 18:32:43 +0530

    117

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -2,7 +2,7 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXUmcMVwzYr44n_5TPSUtyymvESw8yBo8",
+  apiKey: "#",
   authDomain: "myrevents-50ba3.firebaseapp.com",
   databaseURL: "https://myrevents-50ba3.firebaseio.com",
   projectId: "myrevents-50ba3",
